bc69c32c8f5b6cdc0c467304f0de1d0e4df558aa
Marc Terbeck <marc.terbeck@stud.uni-due.de>
2023-04-16 21:37:04 +0200

    added TestClass Kommentra

--- src/main/java/com/defapsim/algorithms/decentral/TestClass.java
+++ src/main/java/com/defapsim/algorithms/decentral/TestClass.java
@@ -3,5 +3,6 @@ package com.defapsim.algorithms.decentral;
 public class TestClass {
     public static void main(String[] args){
       System.out.println("TestPrint");
+      /* Wie gingen nochmal Kommentare */
     }
 }
